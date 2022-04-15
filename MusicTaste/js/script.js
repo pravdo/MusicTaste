bb5839854a0588d9379936b331fa3d27a47da9d5
pkostov <pkostov@2create.studio>
2022-04-16 01:34:23 +0300

    validation changes

--- MusicTaste/js/script.js
+++ MusicTaste/js/script.js
@@ -19,10 +19,8 @@ function myFunction() {
   // contact us
   const nextStep = document.querySelector('#nextStep').addEventListener('click', function (e) {
       e.preventDefault();
-      // Hide first view
       document.getElementById('my_form').style.display = 'none';
   
-      // Show thank you message element
       document.getElementById('thank_you').style.display = 'block';
     });
 
